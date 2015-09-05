d34bce66a96b348e891629a013db3125a7fe8249
Mary Dickson Diaz <mdickson@gmail.com>
2015-09-05 14:49:54 -0700

    added 10 second run

--- lib/fib.rb
+++ lib/fib.rb
@@ -1,3 +1,5 @@
+require 'timeout'
+
 def fibonacci(n)
   n = n.floor
   fail(ArgumentError, "number can't be negative") if n < 0
@@ -9,3 +11,18 @@ def fibonacci(n)
     return (fibonacci(n - 1)) + (fibonacci(n - 2))
   end
 end
+
+def fib_for_10_seconds
+  100.times do | i |
+  puts fibonacci(i)
+  end
+end
+
+@running = true
+  begin
+    Timeout::timeout(10) { fib_for_10_seconds }
+  rescue Timeout::Error
+    stop
+  end
+
+
